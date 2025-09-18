3f6e8087047ef0ef625acb09c4ff39599fe5ccbf
terminator2k2 <48845336+terminator2k2@users.noreply.github.com>
2025-09-18 18:14:43 +0100

    Update buildmenu.sh

--- buildmenu.sh
+++ buildmenu.sh
@@ -92,7 +92,7 @@ if config.has_section('${PROJECT_NAME}'):
 ")
 
 if [[ "${RELEASE_TAG:-}" == "" ]] ; then
-    RELEASE_TAG="unstable-builds"
+    RELEASE_TAG="Purple OSD Background"
 fi
 echo "RELEASE_TAG: ${RELEASE_TAG}"
 
